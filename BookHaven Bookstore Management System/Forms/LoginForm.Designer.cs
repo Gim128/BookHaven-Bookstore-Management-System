075e96ea27ada7d4fc285b6a22d7b3056988d834
Leo Munasinghe <E234957@esoft.academy>
2025-03-20 19:53:02 +0530

    login form code & SecurityHelper complete

--- BookHaven Bookstore Management System/Forms/LoginForm.Designer.cs
+++ BookHaven Bookstore Management System/Forms/LoginForm.Designer.cs
@@ -28,12 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "LoginForm";
+            lblUsername = new Label();
+            lblPassword = new Label();
+            cmbRole = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            cmbRole1 = new ComboBox();
+            btnLogin = new Button();
+            lblLoginPage = new Label();
+            SuspendLayout();
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(185, 150);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(75, 20);
+            lblUsername.TabIndex = 0;
+            lblUsername.Text = "Username";
+            lblUsername.Click += label1_Click;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(185, 207);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(70, 20);
+            lblPassword.TabIndex = 1;
+            lblPassword.Text = "Password";
+            lblPassword.Click += label1_Click_1;
+            // 
+            // cmbRole
+            // 
+            cmbRole.AutoSize = true;
+            cmbRole.Location = new Point(194, 256);
+            cmbRole.Name = "cmbRole";
+            cmbRole.Size = new Size(39, 20);
+            cmbRole.TabIndex = 2;
+            cmbRole.Text = "Role";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(283, 147);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(360, 27);
+            txtUsername.TabIndex = 3;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(283, 200);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(360, 27);
+            txtPassword.TabIndex = 4;
+            // 
+            // cmbRole1
+            // 
+            cmbRole1.FormattingEnabled = true;
+            cmbRole1.Location = new Point(283, 248);
+            cmbRole1.Name = "cmbRole1";
+            cmbRole1.Size = new Size(360, 28);
+            cmbRole1.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(283, 331);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(301, 61);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Login Here";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblLoginPage
+            // 
+            lblLoginPage.AutoSize = true;
+            lblLoginPage.Location = new Point(379, 61);
+            lblLoginPage.Name = "lblLoginPage";
+            lblLoginPage.Size = new Size(90, 20);
+            lblLoginPage.TabIndex = 7;
+            lblLoginPage.Text = "LOGIN PAGE";
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblLoginPage);
+            Controls.Add(btnLogin);
+            Controls.Add(cmbRole1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(cmbRole);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUsername);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblUsername;
+        private Label lblPassword;
+        private Label cmbRole;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private ComboBox cmbRole1;
+        private Button btnLogin;
+        private Label lblLoginPage;
     }
 }
--- BookHaven Bookstore Management System/Forms/LoginForm.cs
+++ BookHaven Bookstore Management System/Forms/LoginForm.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using Newtonsoft.Json;
+using BookHaven_Bookstore_Management_System.Helpers;
 
 namespace BookHaven_Bookstore_Management_System.Forms
 {
-    public partial class LoginForm: Form
+    public partial class LoginForm : Form
     {
+
+        private string connectionString = "Data Source=DESKTOP-1VJ4V7G;Initial Catalog=BookHaven;Integrated Security=True";
         public LoginForm()
         {
             InitializeComponent();
         }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text;
+            string password = txtPassword.Text;
+            string role = cmbRole1.SelectedItem.ToString();
+
+            if(ValidateLogin(username, password, role))
+            {
+                LogLoginAttempt(username,role, true);
+
+                if(role == "Admin")
+                {
+                    AdminDashboardForm adminDashboardForm = new AdminDashboardForm();
+                    adminDashboardForm.Show();
+                }
+                else if(role == "Sales Clerk")
+                {
+                    SalesForm salesForm = new SalesForm();
+                    salesForm.Show();
+                }
+                this.Hide();
+            }
+            else
+            {
+                LogLoginAttempt(username, role, false);
+
+                MessageBox.Show("Invalid login credentials. Please try again.");
+            }
+        }
+
+        
+
+        private bool ValidateLogin(string username, string password, string role)
+        {
+            string query = "SELECT PasswordHash FROM Users WHERE Username = @Username AND Role = @Role";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Role", role);
+                connection.Open();
+                string passwordHash = cmd.ExecuteScalar().ToString();
+                if (passwordHash != null && SecurityHelper.VerifyPasswordHash(password, passwordHash))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void LogLoginAttempt(string username, string role, bool isSuccessful)
+        {
+            var loginAttempt = new
+            {
+                Username = username,
+                Role = role,
+                isSuccessful = isSuccessful,
+                Timestamp = DateTime.Now
+            };
+            string jsonLog = JsonConvert.SerializeObject(loginAttempt, Formatting.Indented);
+
+            System.IO.File.AppendAllText("LoginAttempts.json", jsonLog + Environment.NewLine);
+
+        }
+
+        
     }
 }

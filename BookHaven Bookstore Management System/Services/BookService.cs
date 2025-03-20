4b532c9f8161b617ad2dfe70724cf52475674657
Leo Munasinghe <E234957@esoft.academy>
2025-03-20 22:26:52 +0530

    BookService.cs is created

--- BookHaven Bookstore Management System/Services/BookService.cs
+++ BookHaven Bookstore Management System/Services/BookService.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace BookHaven_Bookstore_Management_System.Services
 {
-    class BookService
+    public class BookService
     {
+        private string connectionString;
+
+       public BookService(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable GetAllBooks()
+        {
+            string query = "SELECT * FROM Books";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
     }
 }
